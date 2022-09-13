4ab5debca256e15f092c073110cc67dec960cfa5
rodrocha444 <rodrocha444@gmail.com>
2022-09-13 19:07:18 -0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { ThemeProvider } from "styled-components";
+import { GlobalStyle } from "./styles/global";
+import { defaultTheme } from "./styles/themes/default";
+
+export function App() {
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <div>
+        llala
+      </div>
+      
+      <GlobalStyle />
+    </ThemeProvider>
+  )
+}
